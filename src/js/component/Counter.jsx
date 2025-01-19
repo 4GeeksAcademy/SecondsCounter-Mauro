f8c18f5a589b7f577345eac053cd524247f7fb84
MauroG07 <71202727+MauroG07@users.noreply.github.com>
2025-01-19 16:35:31 +0000

    CounterCSS

---
+++ src/js/component/Counter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Counter = () => {
+
+    return (
+
+        <div className="container text-white w-75 px-2 m-2">
+            <div className="d-flex justify-content-between fs-1  ">
+            <div className="bg-secondary px-3  rounded"><i class="fa-regular fa-clock"></i></div>            
+            <div className="bg-secondary px-3  rounded">0</div>
+            <div className="bg-secondary px-3  rounded">0</div>
+            <div className="bg-secondary px-3  rounded">0</div>
+            <div className="bg-secondary px-3  rounded">0</div>
+            <div className="bg-secondary px-3  rounded">0</div>
+            <div className="bg-secondary px-3  rounded">0</div>
+            </div>
+
+            
+        </div>
+    );
+};
