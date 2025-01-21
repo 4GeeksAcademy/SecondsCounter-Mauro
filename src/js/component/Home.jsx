391c02ae679148a38ecd8b2e6818af733adda5a2
MauroG07 <71202727+MauroG07@users.noreply.github.com>
2025-01-21 02:35:33 +0000

    CounterMauro

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import { Header } from "./Header.jsx";
 
-//create your first component
+
 const Home = () => {
 	return (
 		<div className="text-center">
-			<Header />
+			<Header />			
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 		</div>
 	);
